877513c5bb76d2b437cf302cffc3ca0aff9e0821
Roarke Gaskill <roarke.gaskill@gmail.com>
2012-10-22 21:14:22 -0500

    refine autocomplete

--- app/js/directives.js
+++ app/js/directives.js
@@ -1,23 +1,43 @@
-	'use strict';
+'use strict';
 
 /* Directives */
 
 
 var module = angular.module('bbToolkit.directives', []);
 
-module.directive('appVersion', ['version', function(version) {
-    return function(scope, elm, attrs) {
-      elm.text(version);
+module.directive('appVersion', ['version', function (version) {
+    return function (scope, elm, attrs) {
+        elm.text(version);
     };
-  }]);
+}]);
 
-module.directive('positionAutocomplete', function($log) {
-    return function(scope, elm, attrs) {
-      // $log.log(scope.player);
-      // $log.log(scope.inning);
-      
-      var expression = attrs.positionAutocomplete;
-      var options = scope.$eval(expression);
-      elm.autocomplete(options);
+
+
+module.directive('positionAutocomplete', function () {
+    return function (scope, element, attrs) {
+        var expression = attrs.positionAutocomplete;
+        var src = scope.$eval(expression);
+        var autoComplete = element.autocomplete(src);
+
+        var fireInput = function (eventType, event, ui) {
+            setTimeout(function () {
+                //ui-state-disabled prevents events from being fired to the autocomplete widget.
+                element.addClass('ui-state-disabled');
+                element.trigger('input');
+                element.removeClass('ui-state-disabled');
+                if (src[eventType]) {
+                    src[eventType](event, ui);
+                }
+            }, 0);
+        };
+
+        autoComplete.data("autocomplete").option('focus', function (event, ui) {
+            fireInput('focus', event, ui);
+        });
+
+        autoComplete.data("autocomplete").option('select', function (event, ui) {
+            fireInput('select', event, ui);
+        });
+
     };
-  });
+});
